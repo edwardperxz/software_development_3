148e875dbd8e6ae22dec0f71e3ec85343f4581de
Alessandra Bonilla <alessandramarie23@gmail.com>
2024-04-15 17:58:28 -0500

    update alessandra files

--- workshop_two/employeeManagementSystem/pryWorkshop_Inheritance/src/pktEmployeeManagementSystem/humanResources.java
+++ workshop_two/employeeManagementSystem/pryWorkshop_Inheritance/src/pktEmployeeManagementSystem/humanResources.java
@@ -1,4 +1,18 @@
 package pktEmployeeManagementSystem;
 
-public class humanResources {
+public class accountant {
+
+    private boolean timecompleted = true;
+    public void generateReports() {
+        System.out.println("Ejecutando método: generatingReports...");
+        System.out.println("Reporte generado con éxito.");
+    }
+    public void reviewDocuments() {
+        System.out.println("Ejecutando método: reviewDocuments...");
+        System.out.println("Documentos revisados con éxito.");
+    }
+    public void prepareTaxDocuments() {
+        System.out.println("Ejecutando método: prepareTaxDocuments...");
+        System.out.println("Documentos de impuestos preparados con éxito.");
+    }
 }
--- workshop_two/employeeManagementSystem/pryWorkshop_Inheritance/src/pktEmployeeManagementSystem/softwareEngineer.java
+++ workshop_two/employeeManagementSystem/pryWorkshop_Inheritance/src/pktEmployeeManagementSystem/softwareEngineer.java
@@ -1,4 +1,16 @@
 package pktEmployeeManagementSystem;
 
 public class softwareEngineer {
+
+        private boolean timeCompleted = true;
+           public void developSoftware() {
+                System.out.println("Ejecutando método: developSoftware");
+        }
+        public void performTesting() {
+            System.out.println("Ejecutando método: performTesting");
+        }
+        public void maintainSystem() {
+            System.out.println("Ejecutando método: maintainSystem");
+        }
 }
+
