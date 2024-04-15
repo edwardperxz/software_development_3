8bd49eaefa37afd83f6600191d41e6dd5c6eec7d
iE1000 <edwardpereztapia@gmail.com>
2024-04-15 17:42:19 -0500

    update financialConsultant

--- workshop_two/employeeManagementSystem/pryWorkshop_Inheritance/src/pktEmployeeManagementSystem/financialConsultant.java
+++ workshop_two/employeeManagementSystem/pryWorkshop_Inheritance/src/pktEmployeeManagementSystem/financialConsultant.java
@@ -1,4 +1,20 @@
 package pktEmployeeManagementSystem;
 
-public class financialConsultant {
+public class financialConsultant extends employeeHalfTime {
+    private boolean FinancialConsultant=true;
+    public financialConsultant(int HoursWorked) {
+        super(HoursWorked);
+        this.FinancialConsultant = FinancialConsultant;
+    }
+    public void adviseClients () {
+        System.out.println("advise");
+    }
+
+    public void conductMarketAnalysis () {
+        System.out.println("analysis");
+    }
+
+    public void prepareFinancialReports () {
+        System.out.println("reports");
+    }
 }
