216851b7384042b6585c32afc3bb30801bd3ac2c
edwardperxz <edwardpereztapia@gmail.com>
2024-06-25 01:35:30 -0500

    update: rutas.page.spect <-- deleted

--- BusNow/busNow_v1.0/src/app/rutas/rutas.page.ts
+++ BusNow/busNow_v1.0/src/app/rutas/rutas.page.ts
@@ -2,20 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-rutas',
   templateUrl: './rutas.page.html',
   styleUrls: ['./rutas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class RutasPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class RutasComponent {}
