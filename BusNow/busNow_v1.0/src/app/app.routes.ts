67406b53833acf452d7f79384e943dd71a3a0b75
edwardperxz <edwardpereztapia@gmail.com>
2024-07-14 01:23:59 -0500

    path actualizados

--- BusNow/busNow_v1.0/src/app/app.routes.ts
+++ BusNow/busNow_v1.0/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomePage } from './home/home.page';
 
 export const routes: Routes = [
-  { path: '', component: HomePage },
   {
     path: '',
     redirectTo: 'login',
