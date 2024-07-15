c8a2440857aaf5f88d333b80f558ceaa0d2c0f83
edwardperxz <edwardpereztapia@gmail.com>
2024-07-15 17:24:01 -0500

    problemas

--- BusNow/busNow_v1.0/src/environments/environment.prod.ts
+++ BusNow/busNow_v1.0/src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC9dtj6PAfSN1d_2gzXxrMv6YdvIh24ftI",
   authDomain: "busnow1.firebaseapp.com",
@@ -16,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-RLPZ6PGT60"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
--- BusNow/busNow_v1.0/src/main.ts
+++ BusNow/busNow_v1.0/src/main.ts
@@ -5,11 +5,8 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment.prod';
 
-if (environment.production) {
-  enableProdMode();
-}
+
 
 bootstrapApplication(AppComponent, {
   providers: [
